9ea3fa31d04153e90ff892e2d30e8997ac5e51ed
codexgh <codexgh@163.com>
2025-01-18 15:42:22 +0800

    node创建线程池来优化性能

---
+++ worker-test/pool-worker.js
@@ -0,0 +1,25 @@
+const { parentPort } = require('node:worker_threads')
+
+function calc(num) {
+  let total = 0;
+  for (let i = 0; i < num; i++) {
+    total += i;
+  }
+  return total
+}
+
+let tunnel, id;
+
+parentPort.on('message', (message) => {
+  if(message.type === 'startup') {
+    id = message.id;
+    tunnel = message.channel;
+    
+    tunnel.on('message', (msg) => {
+      tunnel.postMessage({
+        id,
+        res: calc(msg.value)
+      })
+    })
+  }
+})
